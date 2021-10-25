cd507787f44241267059528ce065b46c8b589b30
hazartilirot <hazartilirot@gmail.com>
2021-10-25 12:57:49 +0300

    Quickselect algorithm returing the value of a specified index. The algorithm works with a partition. Minor syntax changes.

--- QuickSelectWithPartition.js
+++ QuickSelectWithPartition.js
@@ -1,24 +1,32 @@
 /*
 
-Chapter 13 Page 199
+Chapter 13 Page 219
 
 To partition an array is to take a random value from the array—which is then
 called the pivot—and make sure that every number that is less than the pivot
 ends up to the left of the pivot, and that every number greater than the pivot
 ends up to the right of the pivot.
+
+you have an array in random order, and you do not need to sort it,
+but you do want to know the tenth-lowest value in the array, or the
+fifth-highest. This can be useful if we had a lot of test grades and want to
+know what the 25th percentile was, or if we want to find the median grade.
+
+Like Quicksort, Quickselect relies on partitioning, and can be
+thought of as a hybrid of Quicksort and binary search.
 */
 
 
-class QuickSortWithPartition {
 
-	#array = []
+class QuickSelectWithPartition {
+	#array = [];
+	
 	constructor(array = []) {
 		if (!Array.isArray(array)) throw "It is not an array";
 		this.#array = array;
 	}
 	
 	#partition = (leftPointer, rightPointer) => {
-		
 		let pivotIndex = rightPointer;
 		let pivot = this.#array[pivotIndex];
 		
@@ -31,37 +39,34 @@ class QuickSortWithPartition {
 			while (this.#array[rightPointer] > pivot) {
 				rightPointer--;
 			}
-			
 			if (leftPointer >= rightPointer) {
 				break;
 			} else {
 				[this.#array[leftPointer], this.#array[rightPointer]] = 
 					[this.#array[rightPointer], this.#array[leftPointer]];
-				leftPointer++;
-			}
+				leftPointer++
+			}	
 		}
-		
 		[this.#array[leftPointer], this.#array[pivotIndex]] = 
 			[this.#array[pivotIndex], this.#array[leftPointer]]
-		
 		return leftPointer;
 	}
 	
-	quicksort = (leftIndex = 0, rightIndex = this.#array.length - 1) => {
+	quickselect = (nthLowestValue, leftIndex = 0, rightIndex = this.#array.length - 1) => {
 		if (this.#array.length === 0) return;
 		
-		if (rightIndex - leftIndex <= 0) return;
-		
+		if (rightIndex - leftIndex <= 0) return this.#array[leftIndex];
+                 
 		const pivotIndex = this.#partition(leftIndex, rightIndex);
 		
-		this.quicksort(leftIndex, pivotIndex - 1);
-		
-		this.quicksort(pivotIndex + 1, rightIndex);
-		
-		return this.#array;
-	
+		return nthLowestValue < pivotIndex
+		? this.quickselect(nthLowestValue, leftIndex, pivotIndex - 1)
+		: (nthLowestValue > pivotIndex)
+		? this.quickselect(nthLowestValue, pivotIndex + 1, rightIndex)
+		: this.#array[pivotIndex];
 	}
+	
 }
-const array = [0, 5, 2, 1, 6, 3]
+const array = [0, 50, 20, 10, 60, 30];
 
-console.log(new QuickSortWithPartition(array).quicksort())
+console.log(new QuickSelectWithPartition(array).quickselect(0))
